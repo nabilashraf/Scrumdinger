464d161ca107a94d9b4dee003761bbd4ec2eac98
Nabil Ashraf <nabilashraf@chumchum.agency>
2023-01-15 23:43:21 +0500

    Initial Commit

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by macbook on 15/01/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300",systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment:.leading) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600",systemImage: "hourglass.tophalf.fill")
+                }
+            }.accessibilityElement(children:.ignore)
+                .accessibilityLabel("Time Remaining")
+                .accessibilityLabel("10 minutes")
+            Circle().strokeBorder(lineWidth:24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                  Image(systemName: "forward.fill")
+                }.accessibilityLabel("Next Speaker")
+            }
+        }.padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by macbook on 15/01/2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
